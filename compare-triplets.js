d338573f038c35992738d2dd71a75e857a282385
Kyrune <jn-01@centurytel.net>
2020-08-04 10:09:09 -0500

    Create solution compareTriplets

--- compare-triplets.js
+++ compare-triplets.js
@@ -1,1 +1,13 @@
 // Compare the elements in two triplets
+
+function compareTriplets(a, b) {
+    let score = [0, 0];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            score[0]++;
+        } else if (a[i] != b[i]) {
+            score[1]++;
+        }
+    }
+    return score;
+}
