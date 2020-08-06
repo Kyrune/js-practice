db4939aaa2d34021408cc361b74c858a65caf53e
Kyrune <jn-01@centurytel.net>
2020-08-06 11:13:29 -0500

    Create solution

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -1,5 +1,12 @@
 // Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
 function diagonalDifference(arr) {
-
+    let diag1 = 0;
+    let diag2 = 0;
+    const len = arr.length;
+    arr.forEach((element, index, arr) => {
+        diag1 += element[index];
+        diag2 += element[len - 1 - index];
+    })
+    return Math.abs(diag1 - diag2);
 }
