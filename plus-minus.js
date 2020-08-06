40efb473a652dfe076585521c6f3958c44da44ca
Kyrune <jn-01@centurytel.net>
2020-08-06 11:22:31 -0500

    Create solution :)

--- plus-minus.js
+++ plus-minus.js
@@ -2,5 +2,19 @@
 // Print the decimal value of each fraction on a new line with  places after the decimal.
 
 function plusMinus(arr) {
-    
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+    let length = arr.length;
+    arr.forEach( n => {
+        if (n > 0)
+            positive++
+        else if (n < 0) 
+            negative++
+        else    
+            zero++
+    })
+    console.log((positive / length).toFixed(6));
+    console.log((negative / length).toFixed(6));
+    console.log((zero / length).toFixed(6));
 }
