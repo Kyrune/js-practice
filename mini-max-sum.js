df71effc4234a9ed52c723b8156c236750b43d5c
Kyrune <jn-01@centurytel.net>
2020-08-07 10:19:32 -0500

    Create solution

--- mini-max-sum.js
+++ mini-max-sum.js
@@ -2,5 +2,9 @@
 // Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 function miniMaxSum(arr) {
-    
+    arr.sort((a, b) => a - b);
+    let sum1 = String(arr.slice(0, 4).reduce((a, b) => a + b));
+    let sum2 = String(arr.slice(1, 5).reduce((a, b) => a + b))
+    let sumTotal = sum1 + ' ' + sum2;
+    console.log(sumTotal);
 }
