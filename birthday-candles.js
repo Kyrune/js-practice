a9aecda970e46ea1b94c7e9294a61db28d0d0544
Kyrune <jn-01@centurytel.net>
2020-08-10 11:10:37 -0500

    Create solution

--- birthday-candles.js
+++ birthday-candles.js
@@ -1,5 +1,7 @@
 //  Determine the number of candles that are blown out.
 
-function birthdayCandles(ar) {
-    
+function birthdayCandles(n, ar) {
+    let max = Math.max(...ar);
+    let result = ar.filter(candle => candle === max);
+    return result.length;
 }
