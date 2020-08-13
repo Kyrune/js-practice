c04223ace89520cc23e9aad1b94092e76253b54b
Kyrune <jn-01@centurytel.net>
2020-08-13 10:09:53 -0500

    Create solution

--- apple-orange.js
+++ apple-orange.js
@@ -1,5 +1,9 @@
 // Find the respective number of apples and oranges that fall on Sam's house.
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-    
+    let totalApples = apples.filter(value => value + a >= s && value + a <= t).length;
+    let totalOranges = oranges.filter(value => value + b >= s && value + b <= t).length;
+
+    console.log(totalApples);
+    console.log(totalOranges);
 }
