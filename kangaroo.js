40d87fb6fca768f83cd071f4c43da26abb540f31
Kyrune <jn-01@centurytel.net>
2020-08-14 09:36:33 -0500

    Create solution

--- kangaroo.js
+++ kangaroo.js
@@ -1,5 +1,13 @@
 // Can two kangaroo meet after making the same number of jumps?
 
 function kangaroo(x1, v1, x2, v2) {
-    
+    if ((x1 < x2) && (v1 < v2)) {
+        return "NO";
+    } else {
+        if ((v1 != v2) && ((x2 - x1) % (v1 -v2)) == 0) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
 }
