00ecb6adab66596636396d7a8d873db785982894
Kyrune <jn-01@centurytel.net>
2020-08-18 10:46:06 -0500

    Create solution

--- breaking-records.js
+++ breaking-records.js
@@ -3,5 +3,21 @@
 // Points scored in the first game establish her record for the season, and she begins counting from there.
 
 function breakingRecords(scores) {
-    
+    let maxScore = scores[0];
+    let minScore = scores[0];
+
+    let maxScoreIncrease = 0;
+    let minScoreIncrease = 0;
+
+    for (let i = 0; i < scores.length; i++) {
+        if (maxScore < scores[i]) {
+            maxScore = scores[i];
+            maxScoreIncrease++;
+        }
+        if (minScore > scores[i]) {
+            minScore = scores[i];
+            minScoreIncrease++;
+        }
+    }
+    return [maxScoreIncrease, minScoreIncrease];
 }
