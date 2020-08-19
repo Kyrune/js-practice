a7659ada23b9988957a1f92700b4dfbf5faba664
Kyrune <jn-01@centurytel.net>
2020-08-19 10:27:00 -0500

    Add more description

--- birthday-chocolate.js
+++ birthday-chocolate.js
@@ -3,10 +3,18 @@
 // selected such that the length of the segment matches Ron's birth month and the sum of the integers on the 
 // squares is equal to his birth day. You must determine how many ways she can divide the chocolate.
 
+// Complete the birthday function in the editor below. It should return an integer denoting the number of ways Lily can divide the chocolate bar.
+
+// birthday has the following parameter(s):
+
+// s = an array of integers, the numbers on each of the squares of chocolate
+// d = an integer, Ron's birth day
+// m = an integer, Ron's birth month
+
 function birthday(s, d, m) {
     let count = 0;
     let sum = 0;
-    
+
     for (let i = 0; i < m; i++) {
         sum += s[i];
     }
