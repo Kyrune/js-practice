6984381ce650470737d171f451cbef26f57abbf1
Kyrune <jn-01@centurytel.net>
2020-08-20 10:14:38 -0500

    Include pseudo-code

--- divisible-sum-pairs.js
+++ divisible-sum-pairs.js
@@ -1,5 +1,7 @@
 // Count the number of pairs in an array having sums that are evenly divisible by a given number.
 
+// if(ar[i] + ar[j] % k === 0)
+// inner loop starts from j = i + 1, so j is always greater than i
 function divisibleSumPairs(n, ar, k) {
     let count = 0;
 
