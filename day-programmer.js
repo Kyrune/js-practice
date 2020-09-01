68c275427e5739bc98e0dea1a02d80d880bca213
Kyrune <jn-01@centurytel.net>
2020-09-01 09:42:14 -0500

    Create solution

--- day-programmer.js
+++ day-programmer.js
@@ -1,5 +1,15 @@
 // Given year, determine date of the 256th day of the year
 
 function dayOfProgrammer(year) {
-    
+    if (year == 1918) {
+        // Corner case
+        return `26.09.${year}`;
+    } else if (year > 1918) {
+        // Calculating Leap year in Gregorian Calendar
+        const isLeapYear = (year % 4 == 0) && (year % 100 != 0) || year % 400 == 0;
+        return `${isLeapYear ? 12 : 13}.09.${year}`;
+    } else {
+        // Calculating Leap year in Julian Calendar
+        return `${year % 4 ==0 ? 12 : 13}.09.${year}`;
+    }
 }
