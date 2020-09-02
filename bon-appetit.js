62d26368487162688346e4b226619cca58a14d6d
Kyrune <jn-01@centurytel.net>
2020-09-02 10:02:54 -0500

    Include better description

--- bon-appetit.js
+++ bon-appetit.js
@@ -1,1 +1,5 @@
 // Determine whether or not Brian overcharged Anna for their split bill
+
+// Anna and Brian are sharing a meal at a restuarant and they agree to split the bill equally. 
+// Brian wants to order something that Anna is allergic to though, and they agree that Anna won't pay for that item. 
+// Brian gets the check and calculates Anna's portion. You must determine if his calculation is correct.
