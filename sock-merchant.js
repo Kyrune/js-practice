a9ecf9afeac9f771d861315381652a4485b22920
Kyrune <jn-01@centurytel.net>
2020-09-03 10:01:48 -0500

    Include better description

--- sock-merchant.js
+++ sock-merchant.js
@@ -1,1 +1,5 @@
 // How many pairs of socks can John sell?
+
+// John works at a clothing store. He has a large pile of socks that he must pair by color for sale. 
+// Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
