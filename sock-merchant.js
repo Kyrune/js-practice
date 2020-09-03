de44c7840e35c2309822ccb47d99d5ab5e17be68
Kyrune <jn-01@centurytel.net>
2020-09-03 10:05:13 -0500

    Create solution

--- sock-merchant.js
+++ sock-merchant.js
@@ -4,5 +4,15 @@
 // Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
 
 function sockMerchant(n, ar) {
+    let result = 0;
     
+    ar.sort();
+    
+    for (let i = 0; i < n; i++) {
+        if (ar[i] == ar[i + 1]) {
+            i++;
+            result++;
+        }
+    }
+    return result;
 }
