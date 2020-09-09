88f56acdebec8dbe0baf2c24caf91c9d422c874e
Kyrune <jn-01@centurytel.net>
2020-09-09 12:47:14 -0500

    Create solution

--- drawing-book.js
+++ drawing-book.js
@@ -3,5 +3,8 @@
 // Given n and p, find and print the minimum number of pages Brie must turn in order to arrive at page p.
 
 function pageCount(n, p) {
-    
+    const pageTurns = Math.floor(p / 2);
+    const totalTurns = Math.floor(n / 2);
+
+    return Math.min(pageTurns, totalTurns - pageTurns);
 }
