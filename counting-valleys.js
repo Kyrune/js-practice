504fa0e26b92dea3ff4be853d84101d19ce8acab
Kyrune <jn-01@centurytel.net>
2020-09-17 10:25:28 -0500

    Create function shell

--- counting-valleys.js
+++ counting-valleys.js
@@ -7,3 +7,7 @@
 // A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
 // A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
 // Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+function countingValleys(steps, path) {
+    
+}
