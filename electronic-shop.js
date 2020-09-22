db941c5fc214321677d4b8b3523180dda13245e7
Kyrune <jn-01@centurytel.net>
2020-09-22 11:18:49 -0500

    Add description

---
+++ electronic-shop.js
@@ -0,0 +1,4 @@
+// Determine the most expensive Keyboard and USB drive combinations one can purchase within their budget.
+
+// Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to 
+// buy both items, return -1.
