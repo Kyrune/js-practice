454bffed6881040773ca4d4fc60bcbde2679013b
Kyrune <jn-01@centurytel.net>
2020-09-22 11:20:56 -0500

    Create function shell

--- electronic-shop.js
+++ electronic-shop.js
@@ -2,3 +2,7 @@
 
 // Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to 
 // buy both items, return -1.
+
+function getMoneySpent(keyboards, drives, b) {
+    
+}
