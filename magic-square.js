3883426fa86bd80c47166755a9afe102f61a2f62
Kyrune <jn-01@centurytel.net>
2020-09-25 10:17:11 -0500

    Add more description

--- magic-square.js
+++ magic-square.js
@@ -1,5 +1,13 @@
 // Find the minimum cost of converting a 3 by 3 matrix into a magic square.
 
+// We define a magic square to be an nxn matrix of distinct positive integers from 1 to n^2 where the sum of any row, 
+// column, or diagonal of length n is always equal to the same number: the magic constant.
+
+// You will be given a 3x3 matrix s of integers in the inclusive range [1, 9]. We can convert any digit a to any other digit b in the 
+// range [1, 9] at cost of |a - b|. Given s, convert it into a magic square at minimal cost. Print this cost on a new line.
+
+// Note: The resulting magic square must contain distinct integers in the inclusive range [1, 9].
+
 function formingMagicSquare(s) {
     let n = 8;
     let costs = Array(n).fill(0);
