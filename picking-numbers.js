65197c2917a2412c7a977f9053bbb20773aeae56
Kyrune <jn-01@centurytel.net>
2020-09-29 13:12:20 -0500

    Create function shell

--- picking-numbers.js
+++ picking-numbers.js
@@ -1,3 +1,5 @@
 // Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
 
-
+function pickingNumbers(a) {
+    
+}
