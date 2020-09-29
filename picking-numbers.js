5a03035cbb0d7dfb128c6aea50043a3b353344b0
Kyrune <jn-01@centurytel.net>
2020-09-29 13:19:08 -0500

    Create solution

--- picking-numbers.js
+++ picking-numbers.js
@@ -1,5 +1,9 @@
 // Given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
 
 function pickingNumbers(a) {
-    
+    return [...new Set(a)].reduce((c, v) => {
+        return Math.max(c,
+            a.reduce((c1, v1) => c1 += (v1 === v || v1 === v + 1),
+            0));
+    }, 0);
 }
