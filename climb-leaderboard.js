2fd661364f0b8c5948011a0243c6e031936f4715
Kyrune <jn-01@centurytel.net>
2020-09-30 13:46:22 -0500

    Create solution

--- climb-leaderboard.js
+++ climb-leaderboard.js
@@ -4,5 +4,21 @@
 // Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
 
 function climbingLeaderboard(scores, alice) {
-    
+    let positions = [];
+    let distScore = [...new Set(scores)];
+    let aliceIndex = 0;
+    let i = distScore.length - 1;
+
+    alice.forEach(element => {
+         while (i >= 0) {
+             if (element >= distScore[i]) {
+                 i--;
+             } else {
+                 positions.push(i + 2);
+                 break;
+             }
+         }
+         if (i < 0) positions.push(1);
+    });
+    return positions;
 }
