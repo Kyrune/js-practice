8cbbb301c828946c61510ebcb7cd0c690c8f4a54
Kyrune <jn-01@centurytel.net>
2020-10-22 11:42:24 -0500

    Create function shell

--- designer-pdf-viewer.js
+++ designer-pdf-viewer.js
@@ -3,3 +3,6 @@
 // There is a list of 26 character heights aligned by index to their letters. For example, 'a' is at index 0 and 'z' is at index 25. 
 // There will also be a string. Using the letter heights given, determine the area of the rectangle highlight in mm^2 assuming all letters are 1mm wide.
 
+function designerPdfViewer(h, word) {
+    
+}
