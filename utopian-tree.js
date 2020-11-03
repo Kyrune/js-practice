390a47aa41a0a845f7ae0266c8af168d182b14e9
Kyrune <jn-01@centurytel.net>
2020-11-03 13:14:33 -0600

    Create solution

--- utopian-tree.js
+++ utopian-tree.js
@@ -4,5 +4,11 @@
 // A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after n growth cycles?
 
 function utopianTree(n) {
-    
+    let meters = 1;
+    if (n != 0) {
+        for (let j = 1; j <= n; j++) {
+            j % 2 == 0 ? meters++ : meters *= 2;
+        }
+    }
+    return meters
 }
