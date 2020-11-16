597ed6f854756d5d4177f0b279102f157aabb1aa
Kyrune <jn-01@centurytel.net>
2020-11-16 10:45:47 -0600

    Add solution explanation

--- repeated-string.js
+++ repeated-string.js
@@ -1,5 +1,9 @@
 // Find and print the number of letter a's in the first n letters of an infinitely large periodic string.
 
+// 'aCount' is the remaining 'a' characters, while 'total' is the total amount of characters in the given string without the remaining. 
+// The 'remaining' variable is the remaining that doesn't fit into the 'n', so ((n - r) / s.length * c) is the amount of characters without the remainings of 'a' characters. 
+// The remaining must be counted only if it's not greater than the index (i < r). Thus the total amount without the remaining + the remaining 'aCount' is the result.
+
 function repeatedString(s, n) {
     let total = 0;
     let aCount = 0;
