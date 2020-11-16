b9257d558cfe5d32d23ad231f26351633ccdc042
Kyrune <jn-01@centurytel.net>
2020-11-16 10:43:09 -0600

    Create solution

--- repeated-string.js
+++ repeated-string.js
@@ -1,5 +1,19 @@
 // Find and print the number of letter a's in the first n letters of an infinitely large periodic string.
 
 function repeatedString(s, n) {
-    
+    let total = 0;
+    let aCount = 0;
+    let remaining = n % s.length;
+
+    for (let i = s.length; i-- > 0;) {
+        if (s.charAt(i) == 'a') {
+            ++total;
+
+            if (i < remaining) {
+                ++aCount;
+            }
+        }
+    }
+
+    return ((n - remaining) / s.length * total) + aCount;
 }
