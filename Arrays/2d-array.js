7a1b123abb9b80b6170a17bb69be2d850bc5742e
Kyrune <jn-01@centurytel.net>
2020-12-03 10:56:11 -0600

    Create function shell

--- Arrays/2d-array.js
+++ Arrays/2d-array.js
@@ -6,3 +6,7 @@
 // e f g
 // There are 16 hourglasses in arr. An hourglass sum is the sum of an hourglass' values. 
 // Calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass sum. The array will always be 6 x 6.
+
+function hourglassSum(arr) {
+    
+}
