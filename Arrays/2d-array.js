d28b6671a3aaa3ea16e9fbcb6fda293e67845aa4
Kyrune <jn-01@centurytel.net>
2020-12-03 11:02:15 -0600

    Create solution

--- Arrays/2d-array.js
+++ Arrays/2d-array.js
@@ -8,5 +8,17 @@
 // Calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass sum. The array will always be 6 x 6.
 
 function hourglassSum(arr) {
-    
+    let max = -63;
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let sum = arr[i + 1][j + 1];
+            for (let k = 0; k < 3; k++) {
+                sum += arr[i][j + k];
+                sum += arr[i + 2][j + k];
+            }
+            if (sum > max) max = sum;
+        }
+    }
+    return max;
 }
