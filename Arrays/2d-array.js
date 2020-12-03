b8d9de9068b294fc2a8a6bb1b93e642b0be5c190
Kyrune <jn-01@centurytel.net>
2020-12-03 10:55:36 -0600

    Add description

---
+++ Arrays/2d-array.js
@@ -0,0 +1,8 @@
+// Access 2D arrays
+
+// An hourglass in A is a subset of values with indices falling in this pattern in arr's graphical representation:
+// a b c
+//   d
+// e f g
+// There are 16 hourglasses in arr. An hourglass sum is the sum of an hourglass' values. 
+// Calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass sum. The array will always be 6 x 6.
