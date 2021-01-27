2f0f42047586458bdc0a5ef9a684fe6d0ca8ca3b
Kyrune <jn-01@centurytel.net>
2021-01-27 10:38:32 -0600

    Create solution

--- Arrays/left-rotation.js
+++ Arrays/left-rotation.js
@@ -6,5 +6,8 @@
 // Given an array a of n integers and a number, d , perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
 
 function rotLeft(a, d) {
-
+    while (d--) {
+        a.push(a.shift());
+    }
+    return a;
 }
