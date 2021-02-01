5f01f70cfe4b82f23c3c237be1615b8eff699380
Kyrune <jn-01@centurytel.net>
2021-02-01 09:55:02 -0600

    Create solution

--- Arrays/new-year-chaos.js
+++ Arrays/new-year-chaos.js
@@ -7,5 +7,25 @@
 // Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
 
 function minimumBribes(q) {
+    let bribeCount = []
+    let high = 0;
     
+    for (let i = 0; i < q.length; i++) {
+        let val = q[i]
+        bribeCount[val] = 0
+        high = Math.max(val, high)  // update the highest value encountered
+      
+        if (val < high) {
+            // if current value < high value, increment value for all bribeCount indices > val
+            for (let j = val + 1; j < bribeCount.length; j++) {
+                bribeCount[j]++
+                if (bribeCount[j] > 2) {
+                    console.log("Too chaotic")
+                    return;
+                }
+            }
+        }
+    }
+    const sum = bribeCount.reduce((a, b) => a + b, 0)  // sum
+    console.log(sum);
 }
