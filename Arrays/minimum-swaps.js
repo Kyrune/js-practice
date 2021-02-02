b34f6ef30ad0af5f709d9e8ea6ec216245a3ae18
Kyrune <jn-01@centurytel.net>
2021-02-02 09:51:20 -0600

    Add description

---
+++ Arrays/minimum-swaps.js
@@ -0,0 +1,4 @@
+// Return the minimum number of swaps to sort the given array.
+
+// You are given an unordered array consisting of consecutive integers [1, 2, 3, ..., n] without any duplicates. 
+// You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
