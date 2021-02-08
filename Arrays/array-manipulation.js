3399ce8b2d01631f9e78ab99aa1ef3443d5a5188
Kyrune <jn-01@centurytel.net>
2021-02-08 10:43:48 -0600

    Create solution

--- Arrays/array-manipulation.js
+++ Arrays/array-manipulation.js
@@ -4,5 +4,20 @@
 // inclusive. Once all operations have been performed, return the maximum value in the array.
 
 function arrayManipulation(n, queries) {
-    
+    let arr = new Array(n + 1).fill(0); // Edge case for the last element + 1
+    let max = 0;
+  
+    queries.forEach((item) => {
+      arr[item[0]] += item[2];
+      arr[item[1] + 1] -= item[2];
+    });
+  
+    // This is the max optimization -> when computing the perfect sum, compute the max as well
+    arr.reduce((prev, curr) => {
+      const sum = prev + curr;
+      max = sum > max ? sum : max;
+      return sum;
+    });
+  
+    return max;
 }
