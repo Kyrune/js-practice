a497a8c4cfe1202f52603cc5d457a78de69ac959
Kyrune <jn-01@centurytel.net>
2021-02-08 10:28:09 -0600

    Create function shell

--- Arrays/array-manipulation.js
+++ Arrays/array-manipulation.js
@@ -2,3 +2,7 @@
 
 // Starting with a 1-indexed array of zeros and a list of operations, for each operation, add a value to each array element between two given indices, 
 // inclusive. Once all operations have been performed, return the maximum value in the array.
+
+function arrayManipulation(n, queries) {
+    
+}
