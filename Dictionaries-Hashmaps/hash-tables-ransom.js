aa7d77ac6cd4b8f8cb514efc1cf5fe6115b42ce0
Kyrune <jn-01@centurytel.net>
2021-02-09 09:26:25 -0600

    Create solution

--- Dictionaries-Hashmaps/hash-tables-ransom.js
+++ Dictionaries-Hashmaps/hash-tables-ransom.js
@@ -1,5 +1,22 @@
 // Given two sets of dictionaries, tell if one of them is a subset of the other.
 
 function checkMagazine(magazine, note) {
-    
+    let map = { };
+    let replicable = true;
+
+    for (var i of magazine) {
+        map[i] = (map[i] || 0) + 1;
+    }
+
+    for (var i of note) {
+        map[i] =  (map[i] || 0) - 1;
+    }
+
+    for (var i in map) {
+        if (map[i] < 0) {
+            replicable = false;
+            break;
+        }
+    }
+    console.log(replicable ? 'Yes' : 'No');
 }
