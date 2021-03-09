2efd2cb6ed673ec15a9b8a4d6df27e5d7bfb47dc
Kyrune <jn-01@centurytel.net>
2021-03-09 10:23:38 -0600

    Create function shell

--- Dictionaries-Hashmaps/two-strings.js
+++ Dictionaries-Hashmaps/two-strings.js
@@ -1,2 +1,6 @@
 // Given two strings, you find a common substring of non-zero length.
 // Given two strings, determine if they share a common substring. A substring may be as small as one character.
+
+function twoStrings(s1, s2) {
+    
+}
