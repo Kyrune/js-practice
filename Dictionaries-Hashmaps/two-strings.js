d02705398ecbcbb1a3091cea994b3adf87374a57
Kyrune <jn-01@centurytel.net>
2021-03-09 10:32:07 -0600

    Create solution

--- Dictionaries-Hashmaps/two-strings.js
+++ Dictionaries-Hashmaps/two-strings.js
@@ -1,6 +1,16 @@
 // Given two strings, you find a common substring of non-zero length.
 // Given two strings, determine if they share a common substring. A substring may be as small as one character.
 
+// O(n)
 function twoStrings(s1, s2) {
-    
+    let map = {};
+    for (let i of s1) {
+        map[i] = 1;
+    }
+    for (let i of s2) {
+        if (map[i]) {
+            return "YES";
+        }
+    }
+    return "NO";
 }
