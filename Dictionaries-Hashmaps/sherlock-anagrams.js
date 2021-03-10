59ab3ed4b27e4d1c3ab4de7cfe4ce9656986b806
Kyrune <jn-01@centurytel.net>
2021-03-10 13:00:06 -0600

    Create solution

--- Dictionaries-Hashmaps/sherlock-anagrams.js
+++ Dictionaries-Hashmaps/sherlock-anagrams.js
@@ -7,5 +7,23 @@
 // Note: PALINDROMES are not ANAGRAMS.
 
 function sherlockAndAnagrams(s) {
+    let count = 0;
 
+    // Size of the sliding window
+    for (let i = 1; i < s.length; i++) {
+        let found = {};
+
+        // Starting index of the sliding window
+        for (let j = 0; j + i <= s.length; j++) {
+            let substr = s.substr(j, i);
+            substr = substr.split('').sort().join('');
+            if (found[substr]) {
+                count += found[substr];
+                found[substr]++;
+            } else {
+                found[substr] = 1;
+            }
+        }
+    }
+    return count;
 }
