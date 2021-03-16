23857b86785ec46272efff65cf4c8ec2609db62a
Kyrune <jn-01@centurytel.net>
2021-03-16 10:20:19 -0500

    Create function shell

--- Dictionaries-Hashmaps/count-triplets.js
+++ Dictionaries-Hashmaps/count-triplets.js
@@ -2,3 +2,7 @@
 
 // You are given an array and you need to find the number of tripets of indices (i, j, k)  
 // such that the elements at those indices are in geometric progression for a given common ratio r and i < j < k.
+
+function countTriplets (arr, r) {
+    
+}
