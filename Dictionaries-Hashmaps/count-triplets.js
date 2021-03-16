9fa7ccb3f38524fe449c195927bc10669b80240a
Kyrune <jn-01@centurytel.net>
2021-03-16 10:19:59 -0500

    Add description

---
+++ Dictionaries-Hashmaps/count-triplets.js
@@ -0,0 +1,4 @@
+// Return the count of triplets that form a geometric progression.
+
+// You are given an array and you need to find the number of tripets of indices (i, j, k)  
+// such that the elements at those indices are in geometric progression for a given common ratio r and i < j < k.
