14ae69dc3017c18d0613eaaa61c015d5ddbfd77c
Kyrune <jn-01@centurytel.net>
2021-03-23 11:08:41 -0500

    Create solution

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -19,5 +19,19 @@ for (let i = 0; i < n; i++) {
 // 3. Last Element: lastElement, where  is the last element in the sorted array.
 
 function countSwap(a) {
-
+    let swaps = 0;
+    const n = a.length;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                [a[j], a[j + 1]] = [a[j + 1], a[j]];
+                swaps++;
+            }
+        }
+    }
+    
+    console.log(`Array is sorted in ${swaps} swaps.`);
+    console.log(`First Element: ${a[0]}`) 
+    console.log(`Last Element: ${a[n - 1]}`)
 }
