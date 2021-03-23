67b150dc2db2d49de979ab5628f1f59d5d6a21d9
Kyrune <jn-01@centurytel.net>
2021-03-23 11:02:49 -0500

    Change int to let

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -2,9 +2,9 @@
 
 // Consider the following version of Bubble Sort:
 
-for (int i = 0; i < n; i++) {
+for (let i = 0; i < n; i++) {
     
-    for (int j = 0; j < n - 1; j++) {
+    for (let j = 0; j < n - 1; j++) {
         // Swap adjacent elements if they are in decreasing order
         if (a[j] > a[j + 1]) {
             swap(a[j], a[j + 1]);
@@ -19,5 +19,5 @@ for (int i = 0; i < n; i++) {
 // 3. Last Element: lastElement, where  is the last element in the sorted array.
 
 function countSwap(a) {
-    
+
 }
