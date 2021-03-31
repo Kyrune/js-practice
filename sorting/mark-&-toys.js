47b19060c9dc26cab3d2e36fabc1098e19b782b4
Kyrune <jn-01@centurytel.net>
2021-03-31 10:27:23 -0500

    Add description

---
+++ sorting/mark-&-toys.js
@@ -0,0 +1,9 @@
+// You are Mark's best friend and have to help him buy as many toys as possible.
+
+// Mark and Jane are very happy after having their first child. Their son loves toys, so Mark wants to buy some. 
+// There are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to spend, 
+// and he wants to maximize the number of toys he buys with this money. Given a list of toy prices and an amount to spend, 
+// determine the maximum number of gifts he can buy.
+
+// Note Each toy can be purchased only once.
+
