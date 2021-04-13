bb807ae1d25afcafe0a6216fbf861f9d96786224
Kyrune <jn-01@centurytel.net>
2021-04-13 08:18:51 -0500

    Create solution

--- sorting/merge-sort-counting-inversions.js
+++ sorting/merge-sort-counting-inversions.js
@@ -10,5 +10,36 @@
 // the same score, sort those players alphabetically ascending by name.
 
 function countInversions(arr) {
-    
+    let count = 0;
+    function swap(arr1, arr2) {
+        let i = 0;
+        let j = 0;
+        let mergedArr = [];
+        let remainder;
+
+        while (i !== arr1.length && j !== arr2.length) {
+            if (arr1[i] < arr2[j] || arr1[i] === arr2[j]) {
+                mergedArr.push(arr1[i]);
+                i++;
+            } else {
+                mergedArr.push(arr2[j]);
+                count += (arr1.length - i);
+                j++;
+            }
+        }
+        remainder = i >= j ? arr2.slice(j) : arr1.slice(i);
+        return mergedArr.concat(remainder);
+    }
+
+    function mergeSort(arr) {
+        if (arr.length <= 1) return arr;
+        let middle = Math.floor(arr.length / 2);
+
+        let left = mergeSort(arr.slice(0, middle));
+        let right = mergeSort(arr.slice(middle, arr.length));
+
+        return swap(left, right);
+    }
+    mergeSort(arr);
+    return count;
 }
