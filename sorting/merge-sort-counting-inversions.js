c68652b9974b48864943d5800be4102daf7204da
Kyrune <jn-01@centurytel.net>
2021-04-13 08:10:12 -0500

    Add description

---
+++ sorting/merge-sort-counting-inversions.js
@@ -0,0 +1,11 @@
+// Write a Comparator for sorting elements in an array.
+
+// Comparators are used to compare two objects. In this challenge, you'll create a comparator and use it to sort an array. 
+// The Player class is provided in the editor below. It has two fields:
+
+// 1. name: a string
+// 2. score: an integer
+
+// Given an array of n Player objects, write a comparator that sorts them in order of decreasing score. If 2 or more players have
+// the same score, sort those players alphabetically ascending by name.
+
