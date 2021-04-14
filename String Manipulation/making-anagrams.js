6ccb448b3f8c0a3b6bc7619fde88d8a02d18f724
Kyrune <jn-01@centurytel.net>
2021-04-14 10:22:01 -0500

    Create solution

--- String Manipulation/making-anagrams.js
+++ String Manipulation/making-anagrams.js
@@ -8,5 +8,21 @@
 // minimum number of character deletions required to make the two strings anagrams. Determine this number.
 
 function makeAnagram(a, b) {
-    
+    let counter = {};
+    let total = 0;
+
+    Array.from(a).forEach(char => {
+        counter[char] = counter[char] || 0;
+        counter[char]++;
+    })
+    Array.from(b).forEach(char => {
+        counter[char] = counter[char] || 0;
+        counter[char]--;
+    })
+    Object.keys(counter).forEach(k => {
+        if (counter[k] !== 0) {
+            total += Math.abs(counter[k]);
+        }
+    })
+    return total;
 }
