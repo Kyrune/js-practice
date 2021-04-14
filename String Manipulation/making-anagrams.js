4236d5c470c761692e0cc0b486b0b4276bd03642
Kyrune <jn-01@centurytel.net>
2021-04-14 10:16:56 -0500

    Add description

---
+++ String Manipulation/making-anagrams.js
@@ -0,0 +1,9 @@
+// How many characters should one delete to make two given strings anagrams of each other?
+
+// A student is taking a cryptography class and has found anagrams to be very useful. Two strings are anagrams of 
+// each other if the first string's letters can be rearranged to form the second string. In other words, both strings 
+// must contain the same exact letters in the same exact frequency. For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+
+// The student decides on an encryption scheme that involves two large strings. The encryption is dependent on the 
+// minimum number of character deletions required to make the two strings anagrams. Determine this number.
+
