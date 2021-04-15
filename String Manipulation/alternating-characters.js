9bf9e4e576289b76505e259e088ab838cdf0a8e1
Kyrune <jn-01@centurytel.net>
2021-04-15 09:24:06 -0500

    Add description

---
+++ String Manipulation/alternating-characters.js
@@ -0,0 +1,7 @@
+// Calculate the minimum number of deletions required to convert a string into a string
+// in which consecutive characters are different.
+
+// You are given a string containing characters A and B only. Your task is to change it into a string such that there are no matching 
+// adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+
+// Your task is to find the minimum number of required deletions.
