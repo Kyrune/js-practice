ae40a58ff890734c6e853179494176961ca66ca9
Kyrune <jn-01@centurytel.net>
2021-04-20 12:37:01 -0500

    Add description

---
+++ String Manipulation/sherlock-and-valid-string.js
@@ -0,0 +1,6 @@
+// Remove some characters from the string such that the new string's characters have the same frequency.
+
+// Sherlock considers a string to be valid if all characters of the string appear the same number of times. 
+// It is also valid if he can remove just 1 character at 1 index in the string, and the remaining characters will occur 
+// the same number of times. Given a string s, determine if it is valid. If so, return YES, otherwise return NO.
+
