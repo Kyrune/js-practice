c782764ba8e387fb936ed24f3bb29c2205ce7702
Kyrune <jn-01@centurytel.net>
2021-04-28 12:37:40 -0500

    Add description

---
+++ String Manipulation/common-child.js
@@ -0,0 +1,10 @@
+// Given two strings a and b of equal length, what's the longest string(s) that can be constructed such that s is a child to both a and b?
+
+// A string is said to be a child of a another string if it can be formed by deleting 0 or more characters from the other string. 
+// Letters cannot be rearranged. Given two strings of equal length, what's the longest string that can be constructed such that it is a child of both?
+
+// commonChild has the following parameter(s):
+
+// string s1: a string
+// string s2: another string
+// Returns the length of the longest string which is a common child of the input strings
