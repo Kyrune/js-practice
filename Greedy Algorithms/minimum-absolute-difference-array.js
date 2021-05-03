a496303261a4d5b7f1d63c83af08c8e638c234ed
Kyrune <jn-01@centurytel.net>
2021-05-03 10:52:13 -0500

    Add description

--- Greedy Algorithms/minimum-absolute-difference-array.js
+++ Greedy Algorithms/minimum-absolute-difference-array.js
@@ -3,3 +3,8 @@
 // The absolute difference is the positive difference between two values a and b, is written |a - b| or |b - a| and they are equal. If a = 3 and b = 2, |3 - 2| = |2 - 3| = 1. 
 // Given an array of integers, find the minimum absolute difference between any two elements in the array.
 
+// minimumAbsoluteDifference has the following parameter(s):
+// arr[n]: an array of integers.
+
+// Returns
+// The minimum absolute difference found.
